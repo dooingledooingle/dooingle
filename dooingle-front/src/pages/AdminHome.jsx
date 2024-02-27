6a0f234eae636e5a30f08cb021205bda848b5c34
rugii913 <rugii913@gmail.com>
2024-02-28 01:23:15 +0900

    refactor: (프론트)단순하게 8080포트의 일부 페이지로 연결되는 링크 추가
    - 단순하게 8080포트의 일부 페이지로 연결되는 링크 추가

--- dooingle-front/src/pages/AdminHome.jsx
+++ dooingle-front/src/pages/AdminHome.jsx
@@ -4,8 +4,8 @@ export default function AdminHomePage() {
   return (
     <>
       <div><Link to={"/"}>사용자 페이지 홈으로</Link></div>
-      <div><Link to={"#"}>Swagger</Link></div>
-      <div><Link to={"#"}>H2 콘솔</Link></div>
+      <div><Link to={"http://localhost:8080/swagger-ui/index.html#/"}>Swagger</Link></div>
+      <div><Link to={"http://localhost:8080/h2-console"}>H2 콘솔</Link></div>
     </>
   )
 }
