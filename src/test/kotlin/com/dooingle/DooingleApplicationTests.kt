cd1e2465fe0a5b0faf9cc307b4846544a38bb905
umin <adorecamus@gmail.com>
2024-02-27 15:58:35 +0900

    Add: 프로젝트 초기 설정
    - Spring Web, Spring data JPA, H2 DB 적용

---
+++ src/test/kotlin/com/dooingle/DooingleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dooingle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DooingleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
