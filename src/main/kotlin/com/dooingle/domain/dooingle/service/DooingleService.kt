fbc291802c6b9cfd6092b29caa12715c773ca35f
umin <adorecamus@gmail.com>
2024-02-28 16:36:20 +0900

    Add: 실행 시간 측정 aop
    - 메서드 실행 시간 측정 aop 어노테이션 추가
    - 뜨거운 뒹글러 목록 조회 메서드에 어노테이션 적용

--- src/main/kotlin/com/dooingle/domain/dooingle/service/DooingleService.kt
+++ src/main/kotlin/com/dooingle/domain/dooingle/service/DooingleService.kt
@@ -2,12 +2,14 @@ package com.dooingle.domain.dooingle.service
 
 import com.dooingle.domain.dooingle.dto.DooinglerResponse
 import com.dooingle.domain.dooingle.repository.DooingleRepository
+import com.dooingle.global.aop.StopWatch
 import org.springframework.stereotype.Service
 
 @Service
 class DooingleService(
     private val dooingleRepository: DooingleRepository
 ) {
+    @StopWatch
     fun getDooinglerList(condition: String?): List<DooinglerResponse> {
         return when (condition) {
             "hot" -> dooingleRepository.getHotDooinglerList()
