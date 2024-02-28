6c182a37da13b44d0556416c1cb038baf431a931
rugii913 <rugii913@gmail.com>
2024-02-28 16:26:26 +0900

    chore: 기존 build.gradle 종속성 분류 및 주석 추가
    - 기존 build.gradle 종속성 분류 및 주석 추가
      - Kotlin 관련 / layer별 분류
      - 각 라이브러리 종속성이 무엇과 관련되어있는지 주석 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,22 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Kotlin 리플렉션 사용: kotlin-reflect
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
+    // 스프링 부트 구성 프로세서 - spring-boot-configuration-processor
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    // Kotlin-Jackson 모듈: jackson-module-kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // 웹: spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // 데이터(JPA): spring-boot-starter-data-jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // 데이터베이스: h2
+    runtimeOnly("com.h2database:h2")
+
+    // 테스트(스프링 부트 테스트): spring-boot-starter-test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
