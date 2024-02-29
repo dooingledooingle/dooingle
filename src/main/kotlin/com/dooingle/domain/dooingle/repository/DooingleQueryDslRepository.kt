219c39d1e5dc2496150935ceedc327bf4d83111d
rugii913 <rugii913@gmail.com>
2024-02-29 23:43:07 +0900

    refactor: projection을 사용하여 불필요한 join을 줄임
    - projection을 사용하여 불필요한 join을 줄임

--- src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepository.kt
+++ src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepository.kt
@@ -1,12 +1,12 @@
 package com.dooingle.domain.dooingle.repository
 
-import com.dooingle.domain.dooingle.model.Dooingle
+import com.dooingle.domain.dooingle.dto.DooingleResponse
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 
 interface DooingleQueryDslRepository {
 
-    fun getDooinglePageable(cursor: Long?, pageable: Pageable): Slice<Dooingle>
+    fun getDooinglePageable(cursor: Long?, pageable: Pageable): Slice<DooingleResponse>
 
     // TODO 팔로우 기능 구현 후 구현 필요
     // fun getDooinglePageableOfFollows(pageable: Pageable): Slice<Dooingle>
--- src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepositoryImpl.kt
+++ src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.dooingle.domain.dooingle.repository
 
-import com.dooingle.domain.dooingle.model.Dooingle
+import com.dooingle.domain.dooingle.dto.DooingleResponse
 import com.dooingle.domain.dooingle.model.QDooingle
 import com.dooingle.domain.user.model.QUser
+import com.querydsl.core.types.Projections
 import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.Pageable
@@ -15,13 +16,20 @@ class DooingleQueryDslRepositoryImpl(
 
     private val dooingle = QDooingle.dooingle
     private val owner = QUser("ow")
-    private val guest = QUser("gu")
 
-    override fun getDooinglePageable(cursor: Long?, pageable: Pageable): Slice<Dooingle> {
+    override fun getDooinglePageable(cursor: Long?, pageable: Pageable): Slice<DooingleResponse> {
         return queryFactory
-            .selectFrom(dooingle)
-            .join(dooingle.owner, owner).fetchJoin()
-            .join(dooingle.guest, guest).fetchJoin()
+            .select(
+                Projections.constructor(
+                    DooingleResponse::class.java,
+                    owner.name,
+                    dooingle.id,
+                    dooingle.content,
+                    dooingle.createdAt
+                )
+            )
+            .from(dooingle)
+            .join(dooingle.owner, owner) // fetchJoin() 사용하면 에러 발생
             .where(lessThanCursor(cursor))
             .orderBy(dooingle.id.desc())
             .limit(10) // TODO 10을 변수에 저장할 방법 생각하기
@@ -29,6 +37,6 @@ class DooingleQueryDslRepositoryImpl(
             .let { SliceImpl(it) }
     }
 
-    private fun lessThanCursor(cursor: Long?): BooleanExpression?
-        = if (cursor == null) null else dooingle.id.lt(cursor)
+    private fun lessThanCursor(cursor: Long?) = cursor?.let { dooingle.id.lt(it) }
+    // = if (cursor == null) null else dooingle.id.lt(cursor)
 }
