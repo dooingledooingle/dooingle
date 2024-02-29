2745debb1f22192b488e36ab85e369706f4375be
rugii913 <rugii913@gmail.com>
2024-02-29 18:00:54 +0900

    feat: 뒹글 피드 조회 QueryDSL 메서드 초안 작성
    - 뒹글 피드 조회 QueryDSL 메서드 초안 작성

--- src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepository.kt
+++ src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepository.kt
@@ -2,11 +2,12 @@ package com.dooingle.domain.dooingle.repository
 
 import com.dooingle.domain.dooingle.model.Dooingle
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 
 interface DooingleQueryDslRepository {
 
-    fun getDooinglePageable(cursor: Long, pageable: Pageable): Dooingle
+    fun getDooinglePageable(cursor: Long, pageable: Pageable): Slice<Dooingle>
 
     // TODO 팔로우 기능 구현 후 구현 필요
-    // fun getDooinglePageableOfFollows(pageable: Pageable): Dooingle
+    // fun getDooinglePageableOfFollows(pageable: Pageable): Slice<Dooingle>
 }
--- src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepositoryImpl.kt
+++ src/main/kotlin/com/dooingle/domain/dooingle/repository/DooingleQueryDslRepositoryImpl.kt
@@ -4,6 +4,8 @@ import com.dooingle.domain.dooingle.model.Dooingle
 import com.dooingle.domain.dooingle.model.QDooingle
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.domain.SliceImpl
 
 class DooingleQueryDslRepositoryImpl(
     private val queryFactory: JPAQueryFactory,
@@ -11,7 +13,12 @@ class DooingleQueryDslRepositoryImpl(
 
     private val dooingle = QDooingle.dooingle
 
-    override fun getDooinglePageable(cursor: Long, pageable: Pageable): Dooingle {
-        TODO("Not yet implemented")
+    override fun getDooinglePageable(cursor: Long, pageable: Pageable): Slice<Dooingle> {
+        return queryFactory
+            .selectFrom(dooingle)
+            .where(dooingle.id.lt(cursor))
+            .limit(10) // TODO 10을 변수에 저장할 방법 생각하기
+            .fetch()
+            .let { SliceImpl(it) }
     }
 }
