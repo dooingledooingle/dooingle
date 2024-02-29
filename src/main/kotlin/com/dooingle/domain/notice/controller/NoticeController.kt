c28f511b12822eb36abd24b0ff741ee9f4cf2962
umin <adorecamus@gmail.com>
2024-03-01 03:28:45 +0900

    Feat: 공지사항 등록 시 ADMIN 권한으로 인가

--- src/main/kotlin/com/dooingle/domain/notice/controller/NoticeController.kt
+++ src/main/kotlin/com/dooingle/domain/notice/controller/NoticeController.kt
@@ -3,6 +3,7 @@ package com.dooingle.domain.notice.controller
 import com.dooingle.domain.notice.dto.AddNoticeRequest
 import com.dooingle.domain.notice.service.NoticeService
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.net.URI
 
@@ -12,6 +13,7 @@ class NoticeController(
     private val noticeService: NoticeService
 ) {
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     fun addNotice(
         @RequestParam userId: Long, // TODO : @AuthenticationPrincipal 변경
         @RequestBody request: AddNoticeRequest
