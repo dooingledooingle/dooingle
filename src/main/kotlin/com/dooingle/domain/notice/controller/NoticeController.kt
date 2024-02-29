ca9e1d428b3b36bb36ef3bc1c794bc2651677934
umin <adorecamus@gmail.com>
2024-03-01 02:17:22 +0900

    Feat: 공지사항 등록 - 컨트롤러 userId 추후 AuthenticationPrincipal로 수정 필요

---
+++ src/main/kotlin/com/dooingle/domain/notice/controller/NoticeController.kt
@@ -0,0 +1,23 @@
+package com.dooingle.domain.notice.controller
+
+import com.dooingle.domain.notice.dto.AddNoticeRequest
+import com.dooingle.domain.notice.service.NoticeService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+@RestController
+@RequestMapping("/api/notices")
+class NoticeController(
+    private val noticeService: NoticeService
+) {
+    @PostMapping
+    fun addNotice(
+        @RequestParam userId: Long, // TODO : @AuthenticationPrincipal 변경
+        @RequestBody request: AddNoticeRequest
+    ): ResponseEntity<Unit> {
+        val id = noticeService.addNotice(userId, request)
+        return ResponseEntity.created(URI.create("/api/notices/$id")).build()
+    }
+
+}
