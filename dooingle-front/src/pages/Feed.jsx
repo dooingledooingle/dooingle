5ab840f65d509cba357d37d9182b3bed77994dda
rugii913 <rugii913@gmail.com>
2024-03-06 20:18:16 +0900

    add: (프론트)주요 영역 표시
    - 주요 영역 표시

--- dooingle-front/src/pages/Feed.jsx
+++ dooingle-front/src/pages/Feed.jsx
@@ -6,6 +6,22 @@ export default function FeedPage() {
     <>
       <Header />
 
+      <div className="grid grid-cols-12 gap-x-[2.5rem] mx-[8.75rem] h-[4.5rem] ml-40px">
+        <nav className="col-start-1 col-span-3 flex justify-center text-[#5f6368] bg-amber-50">
+          Hello Nav
+        </nav>
+
+        <section className="col-start-4 col-span-6 flex justify-center text-[#5f6368] bg-amber-100">
+          <div>주 콘텐츠 영역</div>
+          {/* 주 콘텐츠 영역 */}
+        </section>
+
+        <aside className="col-start-10 col-span-3 flex justify-center text-[#5f6368] bg-amber-200">
+          <div>뜨거운, 새 뒹글러 목록 표시</div>
+          {/* 뜨거운, 새 뒹글러 목록 표시 */}
+        </aside>
+      </div>
+
       <Link to={"/"}>웰컴 페이지로</Link>
     </>
   )
