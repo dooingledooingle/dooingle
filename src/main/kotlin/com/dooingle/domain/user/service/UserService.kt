1dc9abc5d0b16bd2e2d9ee9703b7b5242a92dd18
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-03-07 11:56:54 +0900

    feat: 프로필 수정 API - S3 버킷에 이미지 저장

--- src/main/kotlin/com/dooingle/domain/user/service/UserService.kt
+++ src/main/kotlin/com/dooingle/domain/user/service/UserService.kt
@@ -1,17 +1,25 @@
 package com.dooingle.domain.user.service
 
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
 import com.dooingle.domain.dooinglecount.service.DooingleCountService
 import com.dooingle.domain.user.dto.DooinglerResponse
 import com.dooingle.domain.user.dto.UpdateProfileRequest
 import com.dooingle.domain.user.dto.UpdateProfileResponse
 import com.dooingle.domain.user.repository.UserRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val dooingleCountService: DooingleCountService
+    private val dooingleCountService: DooingleCountService,
+    private val amazonS3: AmazonS3,
+    @Value("cloud.aws.s3.bucketName") private val bucketName:String
 ) {
     fun getDooinglerList(condition: String?): List<DooinglerResponse> {
         return when (condition) {
@@ -22,14 +30,31 @@ class UserService(
     }
 
     fun updateProfile(userId: Long, request: UpdateProfileRequest, img:MultipartFile?): UpdateProfileResponse {
-        //이미지 null 검사 → s3에 이미지 업로드 → DB에 url 저장 → url 반환
+        var s3Url:String? = null
 
         img?.let {
+            // 1. 이미지에 랜덤 아이디 붙이기
+            val originName = img.originalFilename
+            val ext = originName!!.substring(originName.lastIndexOf("."))
+            val randomId = UUID.randomUUID().toString()
+            val newName = randomId+originName
 
+            // 2. 메서드 인자로 들어갈 메타데이터 생성
+            val metadata = ObjectMetadata()
+            metadata.contentType="image/$ext"
+
+            // 3. S3에 이미지 저장
+            runCatching {
+                amazonS3.putObject(
+                    PutObjectRequest(bucketName, newName, img.inputStream, metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead))
+            }.getOrElse {
+                TODO("예외처리")
+            }
+            s3Url = amazonS3.getUrl(bucketName, newName).toString()
         }
 
-        //repository에 소개, 이미지경로 저장 후 Response dto 반환
+        //DB에서 profile 가져와서 수정한 뒤 저장
         TODO()
     }
-
 }
