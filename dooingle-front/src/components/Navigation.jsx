311980e6602b2b33cbbfbf26c7e598700f40da73
rugii913 <rugii913@gmail.com>
2024-03-07 18:02:46 +0900

    refactor: (프론트)피드 화면 좌측 내비게이션 부분 컴포넌트 분리
    - 피드 화면 좌측 내비게이션 부분 컴포넌트 분리
      - 프로필 이미지 컴포넌트 분리
      - 내비게이션 메뉴 컴포넌트 분리

---
+++ dooingle-front/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+export default function Navigation() {
+  return (
+    <div className="flex flex-col items-center gap-[1rem]">
+      <div>
+        <a href="#">내 뒹글함</a>
+      </div>
+      <div>
+        <a href="#">팔로우하는 뒹글러</a>
+      </div>
+      <div>
+        <a href="#">뒹글 탐색</a>
+      </div>
+    </div>
+  );
+}
--- dooingle-front/src/pages/Feed.jsx
+++ dooingle-front/src/pages/Feed.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Dooingle from "../components/Dooingle.jsx";
+import ProfileImageFrame from "../components/ProfileImageFrame.jsx";
+import Navigation from "../components/Navigation.jsx";
 
 export default function FeedPage() {
   return (
@@ -10,20 +12,8 @@ export default function FeedPage() {
       <div className="grid grid-cols-12 gap-x-[2.5rem] mx-[8.75rem] h-[4.5rem] ml-40px">
         <nav className="col-start-1 col-span-3 flex justify-center text-[#5f6368]">
           <div className="flex flex-col items-center py-[3.75rem] gap-[1.25rem]">
-            <div>
-              <img className="border-[0.125rem] rounded-full w-[7.5rem] h-[7.5rem] object-cover" alt="사용자 프로필 이미지"/>
-            </div>
-            <div className="flex flex-col items-center gap-[1rem]">
-              <div>
-                <a href="#">내 뒹글함</a>
-              </div>
-              <div>
-                <a href="#">팔로우하는 뒹글러</a>
-              </div>
-              <div>
-                <a href="#">뒹글 탐색</a>
-              </div>
-            </div>
+            <ProfileImageFrame />
+            <Navigation />
           </div>
         </nav>
 
