1bf7a7569d1cba8b8147a2d857521075446db8fb
rugii913 <rugii913@gmail.com>
2024-03-07 18:07:47 +0900

    refactor: (프론트)피드 화면 우측 뒹글 페이지 목록 어사이드 컴포넌트 분리
    - 피드 화면 우측 뒹글 페이지 목록 어사이드 컴포넌트 분리

--- dooingle-front/src/pages/Feed.jsx
+++ dooingle-front/src/pages/Feed.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header.jsx";
 import Dooingle from "../components/Dooingle.jsx";
 import ProfileImageFrame from "../components/ProfileImageFrame.jsx";
 import Navigation from "../components/Navigation.jsx";
+import DooinglerListAside from "../components/DooinglerListAside.jsx";
 
 export default function FeedPage() {
   return (
@@ -47,37 +48,7 @@ export default function FeedPage() {
           </div>
         </section>
 
-        <aside
-          className="col-start-10 col-span-3 flex flex-col justify-end items-center text-[#5f6368]">
-          <div className="sticky bottom-0 py-[4.5rem]">
-            <div className="flex flex-col items-center gap-[0.25rem] rounded-br-[0.625rem] border-b-[0.0625rem] border-[#ef7ec2]">
-              <div className="flex flex-col gap-[0.5rem] px-[1rem] py-[0.625rem]">
-                <div className="font-bold text-[#5f6368] text-[1rem]">
-                  <p>새로운 뒹글 페이지</p>
-                </div>
-                <div className="flex flex-col gap-[0.25rem] px-[0.625rem]">
-                  <p>깜이</p>
-                  <p>최유민</p>
-                  <p>곽준선</p>
-                  <p>김다진</p>
-                  <p>노하영</p>
-                </div>
-              </div>
-              <div className="flex flex-col gap-[0.5rem] px-[1rem] py-[0.625rem]">
-                <div className="font-bold text-[#5f6368] text-[1rem]">
-                  <p>뜨거운 뒹글 페이지</p>
-                </div>
-                <div className="flex flex-col gap-[0.25rem] px-[0.625rem]">
-                  <p>깜이</p>
-                  <p>최유민</p>
-                  <p>곽준선</p>
-                  <p>김다진</p>
-                  <p>노하영</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </aside>
+        <DooinglerListAside />
 
         <div className="col-start-1 col-span-12 mt-10">
           <Link to={"/"}>웰컴 페이지로</Link>
