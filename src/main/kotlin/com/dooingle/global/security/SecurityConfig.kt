dc9cdc12c6e9b4765228e0fff8c72bd39a5c6831
umin <adorecamus@gmail.com>
2024-03-07 15:22:16 +0900

    Feat: UsernamePasswordAuthenticationFilter 전에 jwtAuthenticationFilter 추가

--- src/main/kotlin/com/dooingle/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/dooingle/global/security/SecurityConfig.kt
@@ -1,14 +1,18 @@
 package com.dooingle.global.security
 
+import com.dooingle.global.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -21,6 +25,7 @@ class SecurityConfig {
             .authorizeHttpRequests {
                 it.anyRequest().permitAll()
             }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 }
