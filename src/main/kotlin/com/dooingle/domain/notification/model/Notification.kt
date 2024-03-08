01984f089fdc4b3fa9b0ee3bd73e3bf642738cb5
umin <adorecamus@gmail.com>
2024-03-09 00:16:46 +0900

    Add: Notification Entity, NotificationType Enum

---
+++ src/main/kotlin/com/dooingle/domain/notification/model/Notification.kt
@@ -0,0 +1,22 @@
+package com.dooingle.domain.notification.model
+
+import com.dooingle.domain.user.model.SocialUser
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "notification")
+class Notification(
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: SocialUser,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    val notificationType: NotificationType,
+
+    @Column
+    val resourceId: Long
+) {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+}
