27e60465d841caffe9c42ec3b63f01cc2227d99f
rugii913 <rugii913@gmail.com>
2024-03-10 17:55:15 +0900

    feat: 신고 엔티티 작성
    - 신고 엔티티 작성
      - reportReason 부분은 협의가 더 필요함

---
+++ src/main/kotlin/com/dooingle/domain/badreport/model/BadReport.kt
@@ -0,0 +1,28 @@
+package com.dooingle.domain.badreport.model
+
+import com.dooingle.domain.user.model.SocialUser
+import com.dooingle.global.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "bad_report")
+class BadReport(
+    @ManyToOne
+    @JoinColumn(name = "reporter_id")
+    val reporter: SocialUser,
+
+    @Column
+    val reportedTargetType: ReportedTargetType,
+
+    @Column
+    val reportedTargetId: Long,
+
+    @Column
+    val reportReason: String,
+
+) : BaseEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long? = null
+}
