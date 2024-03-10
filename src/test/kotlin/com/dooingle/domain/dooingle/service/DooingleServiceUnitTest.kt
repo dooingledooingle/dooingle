6e4e88dc3a1ef1dfcbc5a3e3ac7aed3a04a08a08
rugii913 <rugii913@gmail.com>
2024-03-10 11:09:20 +0900

    test: DooingleService 중 getDooingleFeeds() 메서드 단위 테스트
    - DooingleService 중 getDooingleFeeds() 메서드 단위 테스트

--- src/test/kotlin/com/dooingle/domain/dooingle/service/DooingleServiceUnitTest.kt
+++ src/test/kotlin/com/dooingle/domain/dooingle/service/DooingleServiceUnitTest.kt
@@ -1,11 +1,9 @@
 package com.dooingle.domain.dooingle.service
 
-import com.dooingle.domain.catch.dto.CatchResponse
-import com.dooingle.domain.catch.model.Catch
 import com.dooingle.domain.catch.repository.CatchRepository
+import com.dooingle.domain.dooingle.controller.DooingleFeedController
 import com.dooingle.domain.dooingle.dto.AddDooingleRequest
-import com.dooingle.domain.dooingle.dto.DooingleAndCatchResponse
-import com.dooingle.domain.dooingle.model.Dooingle
+import com.dooingle.domain.dooingle.dto.DooingleResponse
 import com.dooingle.domain.dooingle.repository.DooingleRepository
 import com.dooingle.domain.dooinglecount.repository.DooingleCountRepository
 import com.dooingle.domain.user.model.SocialUser
@@ -15,13 +13,16 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.result.shouldNotBeSuccess
+import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Slice
 import org.springframework.data.domain.SliceImpl
 import org.springframework.data.repository.findByIdOrNull
 import java.time.ZonedDateTime
+import kotlin.math.min
 
 @DisplayName("DooingleService 단위 테스트")
 class DooingleServiceUnitTest : AnnotationSpec() {
@@ -39,13 +40,24 @@ class DooingleServiceUnitTest : AnnotationSpec() {
 
     lateinit var owner: SocialUser
     lateinit var guest: SocialUser
-    lateinit var dooingleAndCatchResponseSlice: Slice<DooingleAndCatchResponse>
+//    lateinit var dooingleAndCatchResponseSlice: Slice<DooingleAndCatchResponse>
+    lateinit var dooingleResponseList: List<DooingleResponse>
+    lateinit var theLatestSliceOfDooingleResponseList: Slice<DooingleResponse>
+    lateinit var theNextOfLatestSliceOfDooingleResponseList: Slice<DooingleResponse>
 
     @BeforeAll
     fun prepareFixture() {
         owner = getFixtureOfOwner()
         guest = getFixtureOfGuest()
         // dooingleAndCatchResponseSlice = SliceImpl(getFixtureOfDooingleAndCatchResponseList())
+        dooingleResponseList = getFixtureOfDooingleResponseList()
+        theLatestSliceOfDooingleResponseList = SliceImpl(dooingleResponseList.subList(0, DooingleFeedController.PAGE_SIZE))
+        theNextOfLatestSliceOfDooingleResponseList = SliceImpl(
+            dooingleResponseList.subList(
+                DooingleFeedController.PAGE_SIZE,
+                min(DooingleFeedController.PAGE_SIZE + DooingleFeedController.PAGE_SIZE, dooingleResponseList.size),
+            )
+        )
     }
 
     @AfterEach
@@ -53,19 +65,21 @@ class DooingleServiceUnitTest : AnnotationSpec() {
         clearAllMocks()
     }
 
-    // TODO - addDooingleRequest.to(guest, owner)에서 반환하는 dooingle에 임의로 id를 넣을 수 없어서
-    //   return DooingleResponse.from(dooingle) 부분에서 NPE가 발생할 수밖에 없음
-//     @Test fun `뒹글을 등록하는 데에 성공하면, 등록된 뒹글을 확인할 수 있다`() {
-//        every { mockSocialUserRepository.findByIdOrNull(owner.id!!) } returns owner
-//        every { mockDooingleRepository.save(any()) } returns Dooingle(
-//            guest = guest,
-//            owner = owner,
-//            catch = null,
-//            content = "새로 등록 뒹글 내용",
-//        ).also { it.id = (getFixtureOfDooingleList().size + 1).toLong() }
-//        every { mockDooingleCountRepository.findByOwnerId(owner.id!!) } returns dooingleCount
-//        every { dooingleCount.plus() } just Runs
-//     }
+    /*
+     TODO - addDooingleRequest.to(guest, owner)에서 반환하는 dooingle에 임의로 id를 넣을 수 없어서
+       return DooingleResponse.from(dooingle) 부분에서 NPE가 발생할 수밖에 없음
+     @Test fun `뒹글을 등록하는 데에 성공하면, 등록된 뒹글을 확인할 수 있다`() {
+        every { mockSocialUserRepository.findByIdOrNull(owner.id!!) } returns owner
+        every { mockDooingleRepository.save(any()) } returns Dooingle(
+            guest = guest,
+            owner = owner,
+            catch = null,
+            content = "새로 등록 뒹글 내용",
+        ).also { it.id = (getFixtureOfDooingleList().size + 1).toLong() }
+        every { mockDooingleCountRepository.findByOwnerId(owner.id!!) } returns dooingleCount
+        every { dooingleCount.plus() } just Runs
+     }
+     */
     
     @Test
     fun `뒹글을 등록할 때 존재하지 않는 guestId를 전달하면 예외가 발생한다`() {
@@ -98,25 +112,26 @@ class DooingleServiceUnitTest : AnnotationSpec() {
         shouldThrow<Exception> { result.getOrThrow() }
     }
 
-    // TODO DooingleService의 getPage() 메서드 중 DooingleResponse들로 구성된 Slice를 가져온 후에 그 DooingleResponse 안의 catch를 매핑하는 부분 존재함
-    //   확장 함수로 된 이 부분을 mocking 할 수 없어서 단위 테스트 코드 보류
-//    @Test
-//    fun `개인 뒹글 페이지 최신 뒹글+캐치 스크롤을 조회할 때 존재하는 페이지 소유자 id(ownerId)를 전달하면 해당 페이지의 최신 뒹글+캐치를 확인할 수 있다`() {
-//        // given
-//        every { mockSocialUserRepository.findByIdOrNull(owner.id!!) } returns owner
-//        every { mockDooingleRepository.getPersonalPageBySlice(owner, null, any()) } returns dooingleAndCatchResponseSlice
-//
-//        // when
-//        val result = kotlin.runCatching { dooingleService.getPage(owner.id!!, guest.id!!, null) }
-//
-//        // then
-//        result.shouldNotBeSuccess()
-//        shouldThrow<Exception> { result.getOrThrow() }
-//    }
-//
-//    @Test
-//    fun `개인 뒹글 페이지 최신 바로 다음의 뒹글+캐치 스크롤을 조회할 때 존재하는 페이지 소유자 id(ownerId)를 전달하면 해당 페이지의 최신 바로 다음의 뒹글+캐치를 확인할 수 있다`() {
-//    }
+    /*
+     TODO DooingleService의 getPage() 메서드 중 DooingleResponse들로 구성된 Slice를 가져온 후에 그 DooingleResponse 안의 catch를 매핑하는 부분 존재함
+       확장 함수로 된 이 부분을 mocking 할 수 없어서 단위 테스트 코드 보류
+    @Test
+    fun `개인 뒹글 페이지 최신 뒹글+캐치 스크롤을 조회할 때 존재하는 페이지 소유자 id(ownerId)를 전달하면 해당 페이지의 최신 뒹글+캐치를 확인할 수 있다`() {
+        // given
+        every { mockSocialUserRepository.findByIdOrNull(owner.id!!) } returns owner
+        every { mockDooingleRepository.getPersonalPageBySlice(owner, null, any()) } returns dooingleAndCatchResponseSlice
+
+        // when
+        val result = kotlin.runCatching { dooingleService.getPage(owner.id!!, guest.id!!, null) }
+
+        // then
+        result.shouldNotBeSuccess()
+        shouldThrow<Exception> { result.getOrThrow() }
+    }
+    @Test
+    fun `개인 뒹글 페이지 최신 바로 다음의 뒹글+캐치 스크롤을 조회할 때 존재하는 페이지 소유자 id(ownerId)를 전달하면 해당 페이지의 최신 바로 다음의 뒹글+캐치를 확인할 수 있다`() {
+    }
+     */
 
     @Test
     fun `개인 뒹글 페이지 최신 뒹글+캐치 스크롤을 조회할 때 존재하지 않는 페이지 소유자 id(ownerId)를 전달하면 예외가 발생한다`() {
@@ -142,6 +157,36 @@ class DooingleServiceUnitTest : AnnotationSpec() {
         // then // TODO 예외 처리 공통화 이후 더 자세하게 예외 점검할 것
         result.shouldNotBeSuccess()
         shouldThrow<Exception> { result.getOrThrow() }
+    }
+
+    @Test
+    fun `최신 뒹글 피드를 조회하고자 하면 최신 뒹글 피드를 조회할 수 있다`() {
+        // given
+        val cursor: Long? = null // 최신 뒹글 피드 조건
+        val pageRequest = PageRequest.ofSize(DooingleFeedController.PAGE_SIZE)
+        every { mockDooingleRepository.getDooinglesBySlice(cursor, pageRequest) } returns theLatestSliceOfDooingleResponseList
+
+        // when
+        val result = dooingleService.getDooingleFeeds(cursor, PageRequest.ofSize(DooingleFeedController.PAGE_SIZE))
+
+        // then
+        /*result.size shouldBe DooingleFeedController.PAGE_SIZE // 실제 가져오는 크기가 PAGE_SIZE보다 작은 경우 문제*/
+        result.content.first().dooingleId shouldBe theLatestSliceOfDooingleResponseList.first().dooingleId
+    }
+
+    @Test
+    fun `최신 뒹글 피드의 다음 스크롤을 조회하고자 하면 최신 뒹글 피드의 다음 스크롤을 조회할 수 있다`() {
+        // given
+        val cursor = DooingleFeedController.PAGE_SIZE.toLong() // 최신 뒹글 피드 조건
+        val pageRequest = PageRequest.ofSize(DooingleFeedController.PAGE_SIZE)
+        every { mockDooingleRepository.getDooinglesBySlice(cursor, pageRequest) } returns theNextOfLatestSliceOfDooingleResponseList
+
+        // when
+        val result = dooingleService.getDooingleFeeds(cursor, PageRequest.ofSize(DooingleFeedController.PAGE_SIZE))
+
+        // then
+        /*result.size shouldBe DooingleFeedController.PAGE_SIZE // 실제 가져오는 크기가 PAGE_SIZE보다 작은 경우 문제*/
+        result.content.first().dooingleId shouldBe theNextOfLatestSliceOfDooingleResponseList.first().dooingleId
     }
 
     private fun getFixtureOfOwner() = SocialUser(
@@ -176,6 +221,30 @@ class DooingleServiceUnitTest : AnnotationSpec() {
 //        DooingleAndCatchResponse(owner.nickname, 14, "뒹글 내용 1", Catch("캐치 내용", Dooingle(guest, owner, null, "뒹글 내용"),null), ZonedDateTime.now()),
 //        DooingleAndCatchResponse(owner.nickname, 15, "뒹글 내용 1", Catch("캐치 내용", Dooingle(guest, owner, null, "뒹글 내용"),null), ZonedDateTime.now()),
 //    ).sortedBy { it.dooingleId }
+
+    private fun getFixtureOfDooingleResponseList() = listOf<DooingleResponse>(
+        DooingleResponse(owner.nickname, 1, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 2, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 3, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 4, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 5, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 6, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 7, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 8, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 9, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 10, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 11, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 12, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 13, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 14, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 15, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 16, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 17, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 18, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 19, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 20, "뒹글 내용", ZonedDateTime.now()),
+        DooingleResponse(owner.nickname, 21, "뒹글 내용", ZonedDateTime.now()),
+    ).sortedBy { it.dooingleId }
 
     companion object {
         private const val ownerId = 1.toLong()
