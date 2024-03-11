0c68b00c75e1f9d1246c36bc5932fc069b705dc0
umin <adorecamus@gmail.com>
2024-03-12 02:27:06 +0900

    Add: 에러 코드 인터페이스, 공통 에러 코드 Enum

---
+++ src/main/kotlin/com/dooingle/global/exception/CommonErrorCode.kt
@@ -0,0 +1,14 @@
+package com.dooingle.global.exception
+
+import org.springframework.http.HttpStatus
+
+enum class CommonErrorCode(
+    override val httpStatus: HttpStatus, override val message: String
+) : ErrorCode {
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    MODEL_NOT_FOUND(HttpStatus.NOT_FOUND, "모델이 존재하지 않습니다."),
+    NOT_PERMITTED(HttpStatus.FORBIDDEN, "해당 유저는 모델에 대한 권한이 없습니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "올바르지 않은 입력입니다."),
+
+}
