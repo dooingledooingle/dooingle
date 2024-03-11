69c97efa977ed691f76c67d467e2d54a71d8675d
umin <adorecamus@gmail.com>
2024-03-12 02:49:31 +0900

    Add: InvalidParameter, ModelNotFound, NotPermitted 커스텀 예외

---
+++ src/main/kotlin/com/dooingle/global/exception/custom/NotPermittedException.kt
@@ -0,0 +1,13 @@
+package com.dooingle.global.exception.custom
+
+import com.dooingle.global.exception.CommonErrorCode
+
+data class NotPermittedException(
+    val userId: Long,
+    val modelName: String,
+    val modelId: Long
+) : RestApiException(CommonErrorCode.NOT_PERMITTED) {
+
+    override val message = "User(id: $userId)는 $modelName(id: $modelId)에 대한 권한이 없습니다."
+
+}
