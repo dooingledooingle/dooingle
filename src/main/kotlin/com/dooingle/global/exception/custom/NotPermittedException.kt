0ce17a7955a68eef6259dd4156efee92fe1de103
umin <adorecamus@gmail.com>
2024-03-12 12:09:15 +0900

    Refactor: NotPermitted 예외 modelId nullable로 수정

--- src/main/kotlin/com/dooingle/global/exception/custom/NotPermittedException.kt
+++ src/main/kotlin/com/dooingle/global/exception/custom/NotPermittedException.kt
@@ -5,9 +5,10 @@ import com.dooingle.global.exception.CommonErrorCode
 data class NotPermittedException(
     val userId: Long,
     val modelName: String,
-    val modelId: Long
+    val modelId: Long?
 ) : RestApiException(CommonErrorCode.NOT_PERMITTED) {
 
-    override val message = "User(id: $userId)는 $modelName(id: $modelId)에 대한 권한이 없습니다."
-
+    override val message =
+        modelId?.let { "User(id: $userId)는 $modelName(id: $modelId)에 대한 권한이 없습니다." }
+            ?: "User(id: $userId)는 $modelName 에 대한 권한이 없습니다."
 }
