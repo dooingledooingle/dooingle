201e8f858b87ec0e3b803c33e9c965a1ee4da6f8
umin <adorecamus@gmail.com>
2024-03-12 12:20:31 +0900

    Add: ErrorResponse에 errorCode 생성자 추가

--- src/main/kotlin/com/dooingle/global/exception/ErrorResponse.kt
+++ src/main/kotlin/com/dooingle/global/exception/ErrorResponse.kt
@@ -3,4 +3,7 @@ package com.dooingle.global.exception
 data class ErrorResponse(
     val code: String,
     val message: String?
-)
+) {
+    constructor(code: ErrorCode) : this(code.name, code.message)
+
+}
