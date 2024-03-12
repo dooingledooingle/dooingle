a0829f7cddf66e819f7c8cdd5a72f646b2a1a23a
umin <adorecamus@gmail.com>
2024-03-12 12:08:40 +0900

    Add: ConfilctState 예외, 에러 코드

--- src/main/kotlin/com/dooingle/global/exception/CommonErrorCode.kt
+++ src/main/kotlin/com/dooingle/global/exception/CommonErrorCode.kt
@@ -10,5 +10,6 @@ enum class CommonErrorCode(
     MODEL_NOT_FOUND(HttpStatus.NOT_FOUND, "모델이 존재하지 않습니다."),
     NOT_PERMITTED(HttpStatus.FORBIDDEN, "해당 유저는 모델에 대한 권한이 없습니다."),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "올바르지 않은 입력입니다."),
+    CONFLICT_STATE(HttpStatus.CONFLICT, "허용되지 않은 요청입니다."),
 
 }
