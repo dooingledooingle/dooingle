0f96ccfb34e77a6d22c50e2e2319586526a1a294
rugii913 <rugii913@gmail.com>
2024-03-15 01:04:42 +0900

    feat: (프론트) 피드 페이지에서 get 요청
    - 피드 페이지에서 get 요청
    - 오류 발생하던 부분
      - (1) initial state를 적절히 주지 않았음
        - 객체에 어느 정도 depth가 있으면 initial state를 줘야하는 듯하다?(확인 필요)
      - (2) property 이름을 잘못 명시했음

--- dooingle-front/src/components/Dooingle.jsx
+++ dooingle-front/src/components/Dooingle.jsx
@@ -1,15 +1,12 @@
-export default function Dooingle() {
+export default function Dooingle({ownerName, content}) {
   return (
     <div className="flex flex-col p-[0.75rem] gap-[0.5rem]">
       <div className="text-[#456bf5] font-bold">
-        <p>익명의 뒹글러 → ㅇㅇㅇ</p>
+        <p>익명의 뒹글러 → {ownerName?.toString()}</p>
       </div>
       <div className="px-[1.25rem] py-[0.625rem] border-[0.03125rem] border-[#8692ff] rounded-[0.625rem]">
         <div className="text-[#5f6368]">
-          <p>
-            임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용
-            임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용 임시 내용
-          </p>
+          <p>{content}</p>
         </div>
       </div>
     </div>
