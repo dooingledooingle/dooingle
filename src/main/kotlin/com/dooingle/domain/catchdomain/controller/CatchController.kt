34f637567adb592ee40866339e9bfa997a71d77b
kDAJIN <152415476+kDAJIN@users.noreply.github.com>
2024-03-15 11:34:27 +0900

    refactor:캐치 인증인가 적용,불필요한 주석제거

--- src/main/kotlin/com/dooingle/domain/catchdomain/controller/CatchController.kt
+++ src/main/kotlin/com/dooingle/domain/catchdomain/controller/CatchController.kt
@@ -2,10 +2,11 @@ package com.dooingle.domain.catchdomain.controller
 
 import com.dooingle.domain.catchdomain.dto.AddCatchRequest
 import com.dooingle.domain.catchdomain.dto.CatchResponse
-import com.dooingle.domain.catchdomain.dto.DeleteCatchRequest
 import com.dooingle.domain.catchdomain.service.CatchService
+import com.dooingle.global.security.UserPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -21,10 +22,11 @@ class CatchController(
     // 캐치 생성
     @PostMapping
     fun addCatch(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
         @PathVariable dooingleId: Long,
         @RequestBody addCatchRequest: AddCatchRequest
     ): ResponseEntity<CatchResponse> {
-        val response: CatchResponse = catchService.addCatch(dooingleId, addCatchRequest)
+        val response: CatchResponse = catchService.addCatch(dooingleId, userPrincipal.id, addCatchRequest)
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(response)
@@ -33,11 +35,11 @@ class CatchController(
     // 캐치 삭제
     @DeleteMapping("/{catchId}")
     fun deleteCatch(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
         @PathVariable dooingleId: Long,
-        @PathVariable catchId: Long,
-        @RequestBody deleteCatchRequest: DeleteCatchRequest
+        @PathVariable catchId: Long
     ): ResponseEntity<Unit> {
-        catchService.deleteCatch(dooingleId, catchId, deleteCatchRequest)
+        catchService.deleteCatch(dooingleId, catchId, userPrincipal.id)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
