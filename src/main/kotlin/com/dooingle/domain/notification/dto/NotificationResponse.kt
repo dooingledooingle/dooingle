c1907288eab4319f5729a34c1366f1a548c9bd06
umin <adorecamus@gmail.com>
2024-03-15 17:14:42 +0900

    Refactor: 뒹글, 캐치 SSE 알림 반환 타입 NotificationResponse로 수정

--- src/main/kotlin/com/dooingle/domain/notification/dto/NotificationResponse.kt
+++ src/main/kotlin/com/dooingle/domain/notification/dto/NotificationResponse.kt
@@ -1,6 +1,15 @@
 package com.dooingle.domain.notification.dto
 
+import com.dooingle.domain.notification.model.Notification
+
 data class NotificationResponse(
     val notificationType: String,
     val cursor: Long
-)
+) {
+    companion object {
+        fun from(notification: Notification) = NotificationResponse(
+            notificationType = notification.notificationType.toString(),
+            cursor = notification.resourceId
+        )
+    }
+}
