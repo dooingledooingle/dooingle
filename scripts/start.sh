cd98305be6074ca61a229304da713c523795d1f9
rugii913 <rugii913@gmail.com>
2024-03-17 04:48:13 +0900

    add: aws code deploy로 배포 시도 5
    - 스크립트 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 PROJECT_ROOT="/home/ubuntu/dooingle"
 
-JAR_FILE="$PROJECT_ROOT/*.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/dooingle-0.1.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+APP_LOG="$PROJECT_ROOT/code-deploy-log/application.log"
+ERROR_LOG="$PROJECT_ROOT/code-deploy-log/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/code-deploy-log/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/dooingle-0.1.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f "java")
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 PROJECT_ROOT="/home/ubuntu/dooingle"
 
-JAR_FILE="$PROJECT_ROOT/*.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/dooingle-0.1.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+DEPLOY_LOG="$PROJECT_ROOT/code-deploy-log/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f "java")
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
