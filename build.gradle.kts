0bad1078947a4ea1de414061c6c3f2382790c183
umin <adorecamus@gmail.com>
2024-03-21 16:37:14 +0900

    Add: okhttp3, okhttp3-sse

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    // okhttp
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    implementation("com.squareup.okhttp3:okhttp-sse:5.0.0-alpha.12")
 
     // 데이터(JPA): spring-boot-starter-data-jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
