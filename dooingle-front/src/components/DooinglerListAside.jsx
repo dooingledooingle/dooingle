b2ab64ed46f2e9d00487771f45fe8186937e00e1
rugii913 <rugii913@gmail.com>
2024-03-22 12:14:12 +0900

    feat: (프론트)새, 뜨거운 뒹글 페이지 불러오기
    - 새, 뜨거운 뒹글 페이지 불러오기

--- dooingle-front/src/components/DooinglerListAside.jsx
+++ dooingle-front/src/components/DooinglerListAside.jsx
@@ -1,4 +1,24 @@
+import axios from "axios";
+import {BACKEND_SERVER_ORIGIN} from "../env.js";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
+async function fetchUserList(condition) {
+  const response = await axios.get(`${BACKEND_SERVER_ORIGIN}/api/users?condition=${condition}`, {
+    withCredentials: true,
+  });
+  return response.data;
+}
+
 export default function DooinglerListAside() {
+  const [newDooinglerList, setNewDooinglerList] = useState([])
+  const [hotDooinglerList, setHotDooinglerList] = useState([])
+
+  useEffect(() => {
+    fetchUserList("new").then(list => setNewDooinglerList(list))
+    fetchUserList("hot").then(list => setHotDooinglerList(list))
+  }, []);
+
   return (
     <aside
       className="col-start-10 col-span-3 flex flex-col justify-end items-center text-[#5f6368]">
@@ -10,11 +30,7 @@ export default function DooinglerListAside() {
               <p>새로운 뒹글 페이지</p>
             </div>
             <div className="flex flex-col gap-[0.25rem] px-[0.625rem]">
-              <p>깜이</p>
-              <p>최유민</p>
-              <p>곽준선</p>
-              <p>김다진</p>
-              <p>노하영</p>
+              {newDooinglerList.map(newDooingler => <Link key={newDooingler?.userId} to={`personal-dooingles/${newDooingler?.userId}`}>{newDooingler?.nickname}</Link>)}
             </div>
           </div>
           <div className="flex flex-col gap-[0.5rem] px-[1rem] py-[0.625rem]">
@@ -22,11 +38,7 @@ export default function DooinglerListAside() {
               <p>뜨거운 뒹글 페이지</p>
             </div>
             <div className="flex flex-col gap-[0.25rem] px-[0.625rem]">
-              <p>깜이</p>
-              <p>최유민</p>
-              <p>곽준선</p>
-              <p>김다진</p>
-              <p>노하영</p>
+              {hotDooinglerList.map(hotDooingler => <Link key={hotDooingler?.userId} to={`personal-dooingles/${hotDooingler?.userId}`}>{hotDooingler?.nickname}</Link>)}
             </div>
           </div>
         </div>
