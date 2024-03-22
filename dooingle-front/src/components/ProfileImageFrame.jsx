de17a2310db97805b410c1c14cda5820dedbf7ab
rugii913 <rugii913@gmail.com>
2024-03-22 12:11:42 +0900

    feat: (프론트+백)프로필 이미지 불러오기-접속된 사용자의 정보를 바탕으로 불러옴
    - 프로필 이미지 불러오기
      - 접속된 사용자의 정보를 바탕으로 불러오도록 백엔드의 UserController도 수정

--- dooingle-front/src/components/ProfileImageFrame.jsx
+++ dooingle-front/src/components/ProfileImageFrame.jsx
@@ -1,7 +1,27 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {BACKEND_SERVER_ORIGIN} from "../env.js";
+
+async function fetchUserProfile() {
+  const response = await axios.get(`${BACKEND_SERVER_ORIGIN}/api/users/profile`, {
+    withCredentials: true,
+  });
+  return response.data?.imageUrl;
+}
+
 export default function ProfileImageFrame() {
+  const [currentUserImageUrl, setCurrentUserImageUrl] = useState()
+
+  useEffect(() => {
+    fetchUserProfile().then(imageUrl =>
+      imageUrl ? setCurrentUserImageUrl(imageUrl) : setCurrentUserImageUrl("no-image.png"))
+  }, []);
+
   return (
     <div>
-      <img className="border-[0.125rem] rounded-full w-[7.5rem] h-[7.5rem] object-cover" alt="사용자 프로필 이미지"/>
+      <img className="border-[0.125rem] rounded-full w-[7.5rem] h-[7.5rem] object-cover"
+           src={currentUserImageUrl}
+           alt="사용자 프로필 이미지"/>
     </div>
   );
 }
