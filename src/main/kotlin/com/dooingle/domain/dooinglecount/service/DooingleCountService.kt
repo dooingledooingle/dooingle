07db202393c85df2788f955eae6a1abaf126b723
umin <adorecamus@gmail.com>
2024-03-27 03:51:36 +0900

    Refactor: DooingleCountService에서 DooingleCountRedisRepository 의존하도록 수정

--- src/main/kotlin/com/dooingle/domain/dooinglecount/service/DooingleCountService.kt
+++ src/main/kotlin/com/dooingle/domain/dooinglecount/service/DooingleCountService.kt
@@ -1,23 +1,34 @@
 package com.dooingle.domain.dooinglecount.service
 
+import com.dooingle.domain.dooinglecount.repository.DooingleCountRedisRepository
 import com.dooingle.domain.user.dto.DooinglerResponse
-import com.dooingle.domain.dooinglecount.repository.DooingleCountRepository
+import com.dooingle.domain.user.model.SocialUser
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class DooingleCountService(
-    private val dooingleCountRepository: DooingleCountRepository
+    private val dooingleCountRedisRepository: DooingleCountRedisRepository
 ) {
 
-    fun getHotDooinglerList(size: Long): List<DooinglerResponse> {
-        return dooingleCountRepository.getHighCountDooinglers(size)
+    fun plusCount(owner: SocialUser) {
+        dooingleCountRedisRepository.plusCount(owner.id.toString(), owner.nickname)
+        // TODO : userLink로 바꾼다면 owner.id -> owner.userLink로 바꿔야 함
     }
 
-    // 매일 0시 0분 0초에 dooingle_count 모든 데이터 삭제
+    fun getHotDooinglerList(size: Long): List<DooinglerResponse> {
+        return dooingleCountRedisRepository.getHighCountDooinglers(size)?.map {
+            DooinglerResponse(
+                userId = it.substringBefore(":").toLong(),
+                nickname = it.substringAfter(":")
+            )
+        } ?: emptyList()
+    }
+
+    // 매일 0시 0분 0초에 redis 모든 데이터 삭제
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     fun deleteAllDooingleCount() {
-        dooingleCountRepository.deleteAllInBatch()
+        dooingleCountRedisRepository.deleteAll()
     }
 
 }
