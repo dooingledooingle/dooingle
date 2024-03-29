265598735e137eb86baeca37532924846834322d
umin <adorecamus@gmail.com>
2024-03-29 16:43:21 +0900

    Refactor: RedissonClient Config 추가
    - host와 port 환경 변수에서 가져옴

--- src/main/kotlin/com/dooingle/global/redis/RedisConfig.kt
+++ src/main/kotlin/com/dooingle/global/redis/RedisConfig.kt
@@ -1,6 +1,10 @@
 package com.dooingle.global.redis
 
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
 import org.redisson.spring.data.connection.RedissonConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -8,11 +12,22 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") private val host: String,
+    @Value("\${spring.data.redis.port}") private val port: Int,
+) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
-        return RedissonConnectionFactory()
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer()
+            .setAddress("redis://$host:$port")
+        return Redisson.create(config)
+    }
+
+    @Bean
+    fun redisConnectionFactory(redissonClient: RedissonClient): RedisConnectionFactory {
+        return RedissonConnectionFactory(redissonClient)
     }
 
     @Bean
