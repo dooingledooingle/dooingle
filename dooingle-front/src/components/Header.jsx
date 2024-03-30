4c115c097c1218c24a768717da29987133cbc424
rugii913 <rugii913@gmail.com>
2024-03-31 02:40:03 +0900

    refactor: (프론트)사용자 메뉴 드롭다운 메뉴
    - 사용자 메뉴 드롭다운 메뉴
      - 사용자 메뉴 아이콘을 클릭하면 드롭다운 메뉴 표시

--- dooingle-front/src/components/Header.jsx
+++ dooingle-front/src/components/Header.jsx
@@ -1,6 +1,36 @@
 import {Link} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const userMenuRef = useRef();
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    }
+
+    function handleEscKey(event) {
+      if (event.key === "Escape") {
+        setShowMenu(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener("keydown", handleEscKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [userMenuRef]);
+
+  function toggleUserMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <header className="shadow-[inset_0_-0.0625rem_0_0_#d3d3d3]">
       <div className="grid grid-cols-12 gap-x-[2.5rem] mx-[8.75rem] h-[4.5rem] ml-40px">
@@ -11,16 +41,29 @@ export default function Header() {
           </div>
         </div>
 
-        <div className="col-start-10 col-span-3 flex items-center justify-center gap-[1rem] max-h-full pt-[0.5rem]">
-          <Link to={"/notices"}>
+        <div className="col-start-10 col-span-3 flex items-end justify-center gap-[1rem] max-h-full pt-[0.5rem]">
+          <Link to={"/notices"} className="pb-[0.3125rem]">
             <img src="/notice.svg" alt="공지사항 링크" className="h-[2.5rem]"/>
           </Link>
 {/*          <Link to={"#"}> TODO 알림 기능 필요함
             <img src="/notification.svg" alt="알림 링크" className="h-[2.5rem]"/>
           </Link>*/}
-          <Link to={"/my-profile"}>
-            <img src="/profile.svg" alt="프로필 링크" className="h-[2.5rem]"/>
-          </Link>
+          <div ref={userMenuRef} className="relative">
+            <button onClick={toggleUserMenu} className="focus:outlink-none">
+              <img src="/user-menu.svg" alt="사용자 메뉴 드롭다운 메뉴" className="h-[2.5rem]"/>
+            </button>
+            {showMenu && (
+              <ul className="absolute flex flex-col gap-[0.125rem] right-0 w-[8rem]
+              bg-white rounded-b-[0.5rem] border-[#d3d3d3] border-[0.03125rem] border-t-0 shadow-sm pb-[0.25rem]">
+                <li className="px-[1.25rem] py-[0.375rem] hover:bg-[#eaecf9]">
+                  <Link to={"/my-profile"}>프로필 설정</Link>
+                </li>
+                <li className="px-[1.25rem] py-[0.375rem] hover:bg-[#eaecf9]">
+                  <Link to={"#"}>로그아웃</Link>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
 
       </div>
