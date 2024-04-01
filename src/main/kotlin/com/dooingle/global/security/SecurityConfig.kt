c4d609bf2f0b4306785c0d443dff43d7b0a71bcd
rugii913 <rugii913@gmail.com>
2024-04-01 09:05:03 +0900

    refactor: 프로파일별 SecurityConfig 설정 분리
    - 프로파일별 SecurityConfig 설정 분리
      - prod에서는 swagger 등 url에 인증 받은 사용자만 접근 가능하도록 함

--- src/main/kotlin/com/dooingle/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/dooingle/global/security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -28,6 +29,7 @@ class SecurityConfig(
     @Value("\${frontend.domain}") private val frontendOrigin: String, // profile에 따라 허용되는 origin 달라지게 함
 ) {
 
+    @Profile("prod")
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
@@ -47,14 +49,16 @@ class SecurityConfig(
                         "/api/**",
                     ).authenticated()
                     // (3) 요청 url 아래 패턴은 모두 허용(로그인, 인증 서버 콜백 리다이렉트 url 등)
-                    // TODO: "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**"는 개발 환경에서만 인증 불필요하게 만들기
                     .requestMatchers(
                         "/oauth2/login/**",
                         "/oauth2/callback/**",
+                    ).permitAll()
+                    // (4) 요청 url 아래 패턴은 ADMIN만 허용
+                    .requestMatchers(
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
                         "/h2-console/**",
-                    ).permitAll()
+                    ).hasAnyRole("ADMIN") // hasRole() 사용할 경우 에러 - collection이 아닐 때만 가능한 듯
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling {
@@ -82,6 +86,42 @@ class SecurityConfig(
         return http
             .securityMatcher(EndpointRequest.toAnyEndpoint())
             .authorizeHttpRequests { it.anyRequest().permitAll() }
+            .build()
+    }
+
+    @Profile("dev", "local-dev-infra", "local-h2", "local-memory-h2", "test")
+    @Bean
+    fun filterChainForLocal(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .securityMatcher(NegatedRequestMatcher(EndpointRequest.toAnyEndpoint()))
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
+            .headers { it.frameOptions { frameOptionsConfig -> frameOptionsConfig.sameOrigin() } }
+            .authorizeHttpRequests {
+                // (1) dispatcher type이 async인 경우 모두 허용 - CoyoteAdapter asyncDispatch access denied로 검색하여 https://github.com/spring-projects/spring-security/issues/11962 참고 후 임시 조치해둔 것
+                // requestMatchers("/api/notification").permitAll()으로 할 수도 있었겠지만 이 쪽이 더 낫다고 판단함
+                // cf. dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()가  requestMatchers("/api/**").authenticated()보다 뒤에 있는 경우에는 적용이 안 됨
+                it.dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
+                    // (2) 요청 url /api/** 패턴은 모두 인증 필요
+                    .requestMatchers(
+                        "/api/**",
+                    ).authenticated()
+                    // (3) 요청 url 아래 패턴은 모두 허용(로그인, 인증 서버 콜백 리다이렉트 url 등)
+                    .requestMatchers(
+                        "/oauth2/login/**",
+                        "/oauth2/callback/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/h2-console/**",
+                    ).permitAll()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling {
+                it.authenticationEntryPoint(authenticationEntrypoint)
+                it.accessDeniedHandler(accessDeniedHandler)
+            }
             .build()
     }
 }
