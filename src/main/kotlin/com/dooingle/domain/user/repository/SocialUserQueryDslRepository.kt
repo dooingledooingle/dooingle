94ee95b57a9e7a03f1b0e4e0dd132d57084c5829
umin <adorecamus@gmail.com>
2024-04-03 14:00:47 +0900

    Feat: 랜덤 뒹글러 목록 조회
    - SearchDooinglerResponse -> DooinglerWithProfileResponse 변경
    - SocialUserQueryDslRepositoryImpl에 DB에서 랜덤으로 정렬해 limit하는 메서드 추가

--- src/main/kotlin/com/dooingle/domain/user/repository/SocialUserQueryDslRepository.kt
+++ src/main/kotlin/com/dooingle/domain/user/repository/SocialUserQueryDslRepository.kt
@@ -1,12 +1,14 @@
 package com.dooingle.domain.user.repository
 
 import com.dooingle.domain.user.dto.DooinglerResponse
-import com.dooingle.domain.user.dto.SearchDooinglerResponse
+import com.dooingle.domain.user.dto.DooinglerWithProfileResponse
 
 interface SocialUserQueryDslRepository {
     fun getNewDooinglers(size: Long): List<DooinglerResponse>
 
-    fun searchDooinglers(nickname: String): List<SearchDooinglerResponse>
+    fun searchDooinglers(nickname: String): List<DooinglerWithProfileResponse>
+
+    fun getRandomDooinglers(size: Long): List<DooinglerWithProfileResponse>
 
     fun getDooingler(userId: Long): DooinglerResponse
 }
