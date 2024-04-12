001d1f6c3dc7fd5d1b6c8fdb04e89e804f9f271a
rugii913 <rugii913@gmail.com>
2024-04-12 12:55:57 +0900

    add: DooingleService에서 DistributedLock을 사용하지 않게 되면서 필요 없어진 테스트 주석 처리
    DooingleService에서 DistributedLock을 사용하지 않게 되면서 필요 없어진 테스트 주석 처리
    
    - 하루 동안 특정 페이지의 뒹글 개수를 세는 기능 자체를 Redis를 이용하게 되면서 DooingleCount 테이블을 사용할 필요가 없게 됨
    - DooingleService의 addDooingle()에서 DistributedLock의 목적은 DooingleCount와 관련된 경합을 다루는 목적이 컸음
      - DooingleCount에서 같은 owner_id를 같은 레코드를 제어해야했기 때문임
    - 혹시 addDooingle()에서 또 DistributedLock을 필요로 하는 부분이 있는지 점검 후 적용 가능하다면 적용할 것

--- src/test/kotlin/com/dooingle/global/aop/DistributedLockTest.kt
+++ src/test/kotlin/com/dooingle/global/aop/DistributedLockTest.kt
@@ -7,24 +7,16 @@ import com.dooingle.domain.badreport.service.BadReportService
 import com.dooingle.domain.catchdomain.dto.AddCatchRequest
 import com.dooingle.domain.catchdomain.repository.CatchRepository
 import com.dooingle.domain.catchdomain.service.CatchService
-import com.dooingle.domain.dooingle.dto.AddDooingleRequest
 import com.dooingle.domain.dooingle.model.Dooingle
 import com.dooingle.domain.dooingle.repository.DooingleRepository
-import com.dooingle.domain.dooingle.service.DooingleService
 import com.dooingle.domain.dooinglecount.repository.DooingleCountRepository
 import com.dooingle.domain.follow.repository.FollowRepository
 import com.dooingle.domain.follow.service.FollowService
 import com.dooingle.domain.notification.repository.NotificationRepository
-import com.dooingle.domain.notification.service.NotificationService
 import com.dooingle.domain.user.model.SocialUser
 import com.dooingle.domain.user.repository.SocialUserRepository
 import com.dooingle.global.oauth2.provider.OAuth2Provider
-import com.ninjasquad.springmockk.MockkBean
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.runs
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -41,11 +33,10 @@ import java.util.concurrent.TimeUnit
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @ActiveProfiles("test")
 class DistributedLockTest @Autowired constructor(
-    private val dooingleService: DooingleService,
+    // private val dooingleService: DooingleService,
     private val followService: FollowService,
     private val catchService: CatchService,
     private val badReportService: BadReportService,
-    //
     private val dooingleRepository: DooingleRepository,
     private val notificationRepository: NotificationRepository,
     private val socialUserRepository: SocialUserRepository,
@@ -55,9 +46,9 @@ class DistributedLockTest @Autowired constructor(
     private val badReportRepository: BadReportRepository,
 )  {
 
-    @MockkBean private lateinit var notificationService: NotificationService
+    // @MockkBean private lateinit var notificationService: NotificationService
     private val THREAD_COUNT = 2
-    private val BIG_THREAD_COUNT = 100
+    // private val BIG_THREAD_COUNT = 100
 
     @AfterEach
     fun clearData() {
@@ -70,6 +61,12 @@ class DistributedLockTest @Autowired constructor(
         socialUserRepository.deleteAll()
     }
 
+    /*
+    TODO DooingleService에서 DistributedLock을 사용하지 않게 됐기 때문에 이 테스트는 주석 처리함
+     - 하루 동안 특정 페이지의 뒹글 개수를 세는 기능 자체를 Redis를 이용하게 되면서 DooingleCount 테이블을 사용할 필요가 없게 됨
+     - DooingleService의 addDooingle()에서 DistributedLock의 목적은 DooingleCount와 관련된 경합을 다루는 목적이 컸음
+       - DooingleCount에서 같은 owner_id를 같은 레코드를 제어해야했기 때문임
+     - 혹시 addDooingle()에서 또 DistributedLock을 필요로 하는 부분이 있는지 점검 후 적용 가능하다면 적용할 것
     @Test
     fun `특정 유저에게 동시에 많은 뒹글 등록 요청이 들어왔을때 뒹글의 개수와 Count값이 일치해야 한다`(){
         // GIVEN
@@ -102,6 +99,7 @@ class DistributedLockTest @Autowired constructor(
         // THEN
         dooingleCountRepository.findByOwnerId(ownerId = owner.id!!)!!.count shouldBe BIG_THREAD_COUNT
     }
+     */
 
     @Test
     fun `동일한 팔로우 요청이 여러번 들어오면 한번만 처리되어야 한다`(){
